dd764ac3d79e34d757815b4f6de288a437c24d41
lenovo <togramf@live.fr>
2022-02-15 18:00:00 +0100

    ✨ début components

---
+++ src/components/DogCardComponent.vue
@@ -0,0 +1,30 @@
+<template> 
+    <div class="dog-card">
+        <img class="picture" v-bind:src="picture_url">
+        <div>
+            <h2 class="firstname">{{firstname}}</h2>
+            <p class="breed">{{breed}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'DogCard',
+        props: {
+            firstname: {type: String, required: true},
+            breed: String, 
+            picture_url: {type: String, default:"https://my-best-kennel.com/pictures/default.png"}
+        }
+    }
+</script>
+
+<style>
+    .dog-card {
+        display: flex;
+    }
+
+    h2 {
+        font-size: 20px;
+    }
+</style>
---
+++ src/components/DogsGalleryComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dogs-gallery">
+		<DogCard firstname="Pluto" breed="Saint-Hubert" picture_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPRIGF1eudTt1sG59dgMWM8kssiDj-vDk1SA&usqp=CAU"/>
+		<DogCard firstname="Rex" breed="Labrador" picture_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwU4lqKvezQV7u3tOj5YW7iNbVnvdpBw1Xiw&usqp=CAU"/>
+  </div>
+</template>
+
+<script>
+import DogCard from './DogCardComponent.vue'
+
+export default {
+  name: 'DogsGallery',
+	components: {
+		DogCard
+	}
+}
+</script>
+
+<style></style>
