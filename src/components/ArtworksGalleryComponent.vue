eb7b701ffe2823211e5604c626665be465d54e96
lenovo <togramf@live.fr>
2022-03-08 17:15:16 +0100

    ✨ Cours 4 - récup images sur IIIF

--- src/components/ArtworksGalleryComponent.vue
+++ src/components/ArtworksGalleryComponent.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="artwork-gallery">
-    <ArtworkCard title="Titre"    artist ="Artist" date="Date"     picture_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwU4lqKvezQV7u3tOj5YW7iNbVnvdpBw1Xiw&usqp=CAU"/>
-    <ArtworkCard :title="artworksData.data[0].title" :artist="artworksData.data[0].artist_display" :date="artworksData.data[0].date_display" :pictureUrl="artworksData.data[0].image"/>
-    <ArtworkCard :title="artworksData.data[1].title" :artist="artworksData.data[1].artist_display" :date="artworksData.data[1].date_display" :pictureUrl="artworksData.data[1].image"/>
-    <ArtworkCard :title="artworksData.data[2].title" :artist="artworksData.data[2].artist_display" :date="artworksData.data[2].date_display" :pictureUrl="artworksData.data[2].image"/>
+        <ArtworkCard title="Example - Title"    artist ="Artist" date="Date"     picture_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwU4lqKvezQV7u3tOj5YW7iNbVnvdpBw1Xiw&usqp=CAU"/>
+        <ArtworkCard v-for="artwork in artworksData.data" 
+            :key="artwork.id"
+            :title="artwork.title" 
+            :artist="artwork.artist_display" 
+            :date="artwork.date_display" 
+            :image_id="artwork.image_id"
+            :pictureUrl="artwork.image"/>
 
-    <!-- <ArtworkCard :title="artworksData[1].title" :date="artworksData[1].date" :pictureUrl="artworksData[1].picture"/> -->
-
-    <!-- <ArtworkCard title="Pluto2" date="Saint-Hubert" picture_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPRIGF1eudTt1sG59dgMWM8kssiDj-vDk1SA&usqp=CAU"/>
-    <ArtworkCard title="Pluto3" date="Saint-Hubert" picture_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPRIGF1eudTt1sG59dgMWM8kssiDj-vDk1SA&usqp=CAU"/> -->
+        <ArtworkCard title="Pluto2" date="Saint-Hubert" picture_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPRIGF1eudTt1sG59dgMWM8kssiDj-vDk1SA&usqp=CAU"/>
+        <ArtworkCard title="Pluto3" date="Saint-Hubert" picture_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPRIGF1eudTt1sG59dgMWM8kssiDj-vDk1SA&usqp=CAU"/>
   </div>
 </template>
 
