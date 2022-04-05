d4413e0b5bfb0aa976d73ac960dcf8748b3b5eb2
lenovo <togramf@live.fr>
2022-04-05 18:40:16 +0200

    ✨ Cours 8 - update récupération données

--- src/services/api/articAPI.js
+++ src/services/api/articAPI.js
@@ -11,14 +11,20 @@
 // }
 
 // Récupération n oeuvres, seulement les fields intéressants
-const getArtworksData =
+const getArtworks =
     async function() {
   const response = await fetch(
-      'https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_display,image_id&query[term][is_public_domain]=true&page=3&limit=20');
+      'https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date_start,date_display,image_id&query[term][is_public_domain]=true&page=3&limit=40');
   if (response.status == 200)
     return await response.json();
   else
     return Error(response.statusText)
+}
+
+const getArtworksData =
+    async function() {
+  const artworks = await getArtworks()
+  return artworks.data;
 }
 
 export default getArtworksData
